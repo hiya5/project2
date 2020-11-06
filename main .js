75654a53a3bde0854fa301870ce79a96ac48a2f1
hiya5 <72378105+hiya5@users.noreply.github.com>
2020-11-06 11:33:35 +0530

    Add files via upload

---
+++ main .js
@@ -0,0 +1,20 @@
+{
+    function getParagraph1()
+}
+var inputs = [];
+for(var i = 1; i <=6 ;  i++)
+{
+    inputs.push(document.getElementById("para1_input_box" + i ).value);
+}
+inputs.join(".");
+document.getElementById("showparagraph1").innerHTML = inputs.join(".");
+{
+    function getParagraph2()
+}
+var inputs = [];
+for(var i = 1; i <=6 ;  i++)
+{
+    inputs.push(document.getElementById("para2_input_box" + i ).value);
+}
+inputs.join(".");
+document.getElementById("showparagraph2").innerHTML = inputs.join(".");
